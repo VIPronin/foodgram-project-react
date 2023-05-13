5733032cdd315f2cd35ee6577b162c37b15e67a4
Vlad Pronin <proninvladislav@gmail.com>
2023-05-14 00:49:51 +0300

    urls3

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import include, path
+from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 
-from .views import IngredientViewSet, RecipeViewSet, TagViewSet, UsersViewSet
+from .views import (IngredientViewSet, RecipeViewSet,
+                    SuSubscriptionCreateDeleteAPIView, 
+                    SubscriptionsViewSet, TagViewSet, UsersViewSet)
 
 router_v1 = DefaultRouter()
 router_v1.register('recipes', RecipeViewSet, basename='recipes')
@@ -11,6 +14,20 @@ router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
 
 
 urlpatterns = [
+    path(
+        'docs/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='docs'
+    ),
+    path(
+        'users/<int:id>/subscribe/',
+        SuSubscriptionCreateDeleteAPIView.as_view(),
+        name='subscribe'),
+    path(
+        'users/subscriptions/',
+        SubscriptionsViewSet.as_view({'get': 'list'}),
+        name='subscriptions'
+    ),
     path('', include('djoser.urls')),
     path('', include(router_v1.urls)),
     path('auth/', include('djoser.urls.authtoken')),
